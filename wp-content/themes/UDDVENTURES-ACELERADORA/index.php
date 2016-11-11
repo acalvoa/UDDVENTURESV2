07d39c0be77609da304b5b4ba95dbd3d4f462cbf
Angelo Calvo A <angelo.calvoa@gmail.com>
2016-11-11 09:43:01 -0300

    NUEVAS CORECCIONES

--- wp-content/themes/UDDVENTURES-ACELERADORA/index.php
+++ wp-content/themes/UDDVENTURES-ACELERADORA/index.php
@@ -5,10 +5,10 @@
 	<?php include("extend/space/index.php");?>
 	<?php include("extend/main/index.php");?>
 	<?php include("extend/postula/index.php");?>
+	<?php include("extend/noticias/index.php");?>
 	<?php include("extend/emprendedores/index.php");?>
 	<?php include("extend/redes/index.php");?>
 	<?php include("extend/recursos/index.php");?>
-	<?php include("extend/noticias/index.php");?>
 	<?php include("extend/eventos/index.php");?>
 	<!-- /section -->
 </main>
